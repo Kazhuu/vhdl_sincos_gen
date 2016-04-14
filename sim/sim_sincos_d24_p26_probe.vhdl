15518ce6f79229f15fd4a3ba926990850c618c7f
Joris van Rantwijk <joris@jorisvr.nl>
2016-04-14 23:06:21 +0200

    * Add test-bench for superficial check of 24-bit sine generator. * Minor improvement in test-bench for 18-bit sine generator.

--- sim/sim_sincos_d24_p26_probe.vhdl
+++ sim/sim_sincos_d24_p26_probe.vhdl
@@ -1,5 +1,5 @@
 --
--- Top-level simulation test bench to probe sincos_gen_d18_p20
+-- Top-level simulation test bench to probe sincos_gen_d24_p26
 -- at a couple of test inputs.
 --
 -- Joris van Rantwijk
@@ -9,35 +9,37 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity sim_sincos_d18_p20_probe is
+entity sim_sincos_d24_p26_probe is
 
 end entity;
 
-architecture arch of sim_sincos_d18_p20_probe is
+architecture arch of sim_sincos_d24_p26_probe is
 
     type input_list_type is array(natural range <>) of integer;
-    constant input_list: input_list_type(0 to 7) := (
+    constant input_list: input_list_type(0 to 9) := (
         0,          -- 0 rad
-        12345,      -- 0.073972 rad
-        131072,     -- 45 degrees
-        166886,     -- 0.999999 rad
-        262144,     -- 90 degrees
-        345678,     -- 2.071341 rad
-        667544,     -- 3.999999 rad
-        1019449 );  -- 350.0000 degrees
+        12345,      -- 0.00115582 rad
+        1234567,    -- 0.11558850 rad
+        8388608,    -- 45 degrees
+        10680707,   -- 0.99999996 rad
+        16777216,   -- 90 degrees
+        20304050,   -- 1.90100236 rad
+        34567890,   -- 3.23647944 rad
+        42722830,   -- 4.00000003 rad
+        65244729 ); -- 350.000001 degrees
 
     signal clk_enable:  boolean := false;
     signal clk:         std_logic;
     signal clk_en:      std_logic;
-    signal in_phase:    unsigned(19 downto 0);
-    signal out_sin:     signed(17 downto 0);
-    signal out_cos:     signed(17 downto 0);
+    signal in_phase:    unsigned(25 downto 0);
+    signal out_sin:     signed(23 downto 0);
+    signal out_cos:     signed(23 downto 0);
 
 begin
 
     clk <= (not clk) after 2 ns when clk_enable else '0';
 
-    gen0: entity work.sincos_gen_d18_p20
+    gen0: entity work.sincos_gen_d24_p26
         port map (
             clk         => clk,
             clk_en      => clk_en,
@@ -61,7 +63,7 @@ begin
         for i in 0 to input_list'high+6 loop
 
             if i <= input_list'high then
-                in_phase    <= to_unsigned(input_list(i), 20);
+                in_phase    <= to_unsigned(input_list(i), 26);
             end if;
 
             if i >= 6 then
